c3f2f8fe648b4900d1f0a4ee5eacb806b17573b0
Extakuma <soccer.takuma.1116@icloud.com>
2021-09-27 11:45:32 +0000

    応用課題4 Finish

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
   end
 
 
-  resources :users, only: [:index, :show, :edit, :update, :create]
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 
 end
