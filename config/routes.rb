e52da881cbdfb3c244b9b8f4713c48aaef833d2d
Extakuma <soccer.takuma.1116@icloud.com>
2021-09-28 09:22:46 +0000

    応用課題５ Finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'search/search'
   devise_for :users
   root to: 'homes#top'
   get '/home/about' => 'homes#about'
+  get '/search' => 'search#search'
 
   resources :books, only: [:index, :create, :show, :edit, :update, :destroy] do
 
